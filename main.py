4815f009bbcde6cdab9ca5d0fc06140091c09b3e
Peter Schwenk <paschwenk@gmail.com>
2025-12-07 17:17:39 -0500

    After "Token Metadata"

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
     print("Hello from python-agent!")
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print("User Prompt:", user_prompt)
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_prompt
         )
-    print("Response from Gemini API:")
-    print(response.text)
+    if response.usage_metadata:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response from Gemini API:")
+        print(response.text)
+    else:
+        raise RuntimeError("No usage metadata found in the response.")
 
 
 if __name__ == "__main__":
