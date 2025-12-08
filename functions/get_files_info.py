6b018dc4df91e09aa102f24e3d4873d2b35be2ff
Peter Schwenk <paschwenk@gmail.com>
2025-12-07 20:59:30 -0500

    After "Get Files" lesson

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+from functions.is_subdir import is_subdir
+
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    print(f"Result for {f"'{directory}'" if directory != '.' else 'current'} directory:")
+    if not is_subdir(working_directory, full_path):
+        return f'\tError: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.exists(full_path):
+        return f'\tError: The directory "{directory}" does not exist.'
+    if not os.path.isdir(full_path):
+        return f'\tError: "{directory}" is not a directory.'
+    files_info = []
+    items = os.listdir(full_path)
+    if items:
+        for item in os.listdir(full_path):
+            item_info = f"\t- {item}: file_size={os.path.getsize(os.path.join(full_path, item))} bytes, is_dir={os.path.isdir(os.path.join(full_path, item))}"
+            files_info.append(item_info)
+    return "\n".join(files_info)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ test_get_files_info.py
@@ -0,0 +1,13 @@
+from functions.get_files_info import get_files_info
+
+def test_get_files_info():
+    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "pkg"))
+    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "../"))
+
+def main():
+    test_get_files_info()
+
+if __name__ == "__main__":
+    main()
